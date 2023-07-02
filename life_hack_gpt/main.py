230e52f38e8ef37e2064dc5847ce844dd741821b
Martin Studna <martin.studna2@gmail.com>
2023-07-02 14:33:54 +0200

    init

--- life_hack_gpt/main.py
+++ life_hack_gpt/main.py
@@ -1,6 +1,7 @@
 from argparse import Namespace, ArgumentParser
 import os
 from time import sleep
+import numpy as np
 
 import openai
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
@@ -16,10 +17,31 @@ import sys
 import speech_recognition as sr
 import sounddevice as sd
 from TTS.api import TTS
+import whisper
+
+stt = whisper.load_model("medium") 
+
+def inference(audio):
+    
+    buffer = whisper.pad_or_trim(audio[:,0])
+    mel = whisper.log_mel_spectrogram(buffer).to(stt.device)
+
+    options = whisper.DecodingOptions(language="en", fp16=False)
+    result = whisper.decode(stt, mel, options)
+    
+    print(result.text)
+    return result.text
+
+
 
 def main(args: Namespace) -> None:
   load_dotenv()
   os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+  
+  if not os.path.exists(args.data_dir):
+    os.makedirs(args.data_dir)
+    f = open(os.path.join(args.data_dir, "main.txt"), "w")
+    f.write("SAMPLE DATA")
   
   PERSIST = False
   
@@ -54,40 +76,36 @@ def main(args: Namespace) -> None:
   model_name = TTS.list_models()[0]
   tts = TTS(model_name)
   
+  duration = 8
+  fs = 16000
+
 
   # Otherwise, we start a loop where we ask for user input.
-  with sr.Microphone() as source:
-    while True:
-      print("Listening...")
-      
-      r.adjust_for_ambient_noise(source)
-      
-      audio = r.listen(source, timeout=10)
-      
-      # Perform speech recognition
-      try:
-          text = r.recognize_google(audio)
-          print("Recognized text:", text)
-          query = text
-      except sr.UnknownValueError:
-          print("Speech recognition could not understand audio")
-      except sr.RequestError as e:
-          print("Could not request results from Google Speech Recognition service; {0}".format(e))
-      
-      
-      if not query:
-        query = input("Prompt: ")
-      if query in ['quit', 'q', 'exit']:
-        sys.exit()
-      result = chain({"question": query, "chat_history": chat_history})
-      
-      wav = tts.tts(result["answer"], speaker=tts.speakers[0], language=tts.languages[0])
+  #with sr.Microphone() as source:
+  while True:
+    print("Listening...")
+    
+    #r.adjust_for_ambient_noise(source)
+    audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype=np.float32)
+    sd.wait()
+    
+    
+    #audio = r.listen(source, timeout=10)
+    query = inference(audio)
 
-      sd.play(wav, samplerate=23000)
-      sd.wait()  
+    if not query:
+      query = input("Prompt: ")
+    if query in ['quit', 'q', 'exit']:
+      sys.exit()
+    result = chain({"question": query, "chat_history": chat_history})
+    
+    wav = tts.tts(result["answer"], speaker=tts.speakers[1], language=tts.languages[0])
 
-      chat_history.append((query, result['answer']))
-      query = None
+    sd.play(wav, samplerate=19000)
+    sd.wait()  
+
+    chat_history.append((query, result['answer']))
+    query = None
 
 
 if __name__ == '__main__':
